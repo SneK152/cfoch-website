d549ab7cb3fa86fb17bd85e6c3331574bee7883c
snek152 <kakanisnehil@gmail.com>
2022-04-30 14:46:52 -0700

    polishing the navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,72 +1,65 @@
-import { Disclosure } from "@headlessui/react";
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Disclosure, Popover } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import navLinks from "@lib/data/navLinks";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+  const router = useRouter();
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Popover as="nav" className="bg-lightgreen relative">
       {({ open }) => (
         <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Popover.Button className="inline-flex items-center justify-center p-2 rounded-md text-darkgreen focus:outline-none focus:ring-2 focus:ring-inset focus:ring-darkgreen">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
                     <MenuIcon className="block h-6 w-6" aria-hidden="true" />
                   )}
-                </Disclosure.Button>
+                </Popover.Button>
               </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">CFOCH</div>
-                <div className="hidden sm:block sm:ml-6">
-                  <div className="flex space-x-4">
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-between">
+                <p className="font-bold font-display text-xl">CFOCH</p>
+                <div className="hidden sm:block">
+                  <div className="flex gap-4 items-center h-full">
                     {navLinks.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={`
-                          "px-3 py-2 rounded-md text-sm font-medium"
-                        `}
-                      >
-                        {item.name}
-                      </a>
+                      <Link key={item.name} href={item.href}>
+                        <a
+                          className={`${
+                            router.pathname === item.href ? "font-bold" : ""
+                          } rounded-md text-base`}
+                        >
+                          {item.name}
+                        </a>
+                      </Link>
                     ))}
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-              </div>
             </div>
           </div>
-
-          <Disclosure.Panel className="sm:hidden">
+          <Popover.Panel className="sm:hidden absolute bg-lightgreen w-full">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navLinks.map((item) => (
-                <Disclosure.Button
+                <Popover.Button
                   key={item.name}
                   as="a"
                   href={item.href}
-                  className={`
-                    "block px-3 py-2 rounded-md text-base font-medium"
-                  `}
+                  className={`block text-base ${
+                    router.pathname === item.href ? "font-bold" : ""
+                  }`}
                 >
                   {item.name}
-                </Disclosure.Button>
+                </Popover.Button>
               ))}
             </div>
-          </Disclosure.Panel>
+          </Popover.Panel>
         </>
       )}
-    </Disclosure>
+    </Popover>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkgreen: colors.green[800],
+        lightgreen: colors.emerald[400],
+        blue: {
+          DEFAULT: colors.cyan[500],
+        },
+        black: "#031926",
+      },
+    },
     fontFamily: {
       sans: ["Hind Madurai", "sans"],
       display: ["Overpass", "sans"],
