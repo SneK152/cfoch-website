5dab9125b3c9650e93773173acddd19d645b6e91
snek152 <kakanisnehil@gmail.com>
2022-05-01 15:22:11 -0700

    tweaking carousel

--- components/pages/home/About.tsx
+++ components/pages/home/About.tsx
@@ -17,24 +17,24 @@ export default function About() {
             loading="eager"
             quality={100}
             objectFit="cover"
-            className="select-none"
+            className="select-none rounded-xl"
           />
         ),
       })),
       disableGestures: true,
     });
   return (
-    <div className="w-full h-96 grid grid-cols-3">
-      <div className="relative overflow-hidden">
+    <div className="w-full h-96 grid grid-cols-3 gap-3 p-10">
+      <div className="relative overflow-hidden bg-white rounded-xl">
         <button
-          className="absolute h-full top-0 bottom-0 left-0 z-[1] flex items-center justify-center sm:w-[5%] w-[10%] p-0 text-black text-center bg-none border-0"
+          className="absolute h-full top-0 bottom-0 left-0 z-[1] flex items-center justify-center sm:w-[10%] w-[15%] p-0 text-white text-center bg-none border-0"
           onClick={slideToPrevItem}
         >
           <ChevronLeftIcon className="w-9 h-9 p-1" />
         </button>
         {carouselFragment}
         <button
-          className="absolute h-full top-0 bottom-0 right-0 z-[1] flex items-center justify-center sm:w-[5%] w-[10%] p-0 text-black text-center bg-none border-0"
+          className="absolute h-full top-0 bottom-0 right-0 z-[1] flex items-center justify-center sm:w-[10%] w-[15%] p-0 text-white text-center bg-none border-0"
           onClick={slideToNextItem}
         >
           <ChevronRightIcon className="w-9 h-9 p-1" />
