37a29a79eda7d9b2d180a28a9acd9a09921dfdca
snek152 <kakanisnehil@gmail.com>
2022-04-30 19:25:47 -0700

    changing background color

--- components/layout/Container.tsx
+++ components/layout/Container.tsx
@@ -13,9 +13,7 @@ export default function Container(props: ContainerProps) {
       <Navbar />
       <div className="bg-lightgreen h-16"></div>
       <Meta title={props.title} />
-      <div className="bg-gradient-to-r from-blue to-lightgreen">
-        {props.children}
-      </div>
+      <div className="bg-blue">{props.children}</div>
     </>
   );
 }
