611d0d43f031f2b2735a53aea6e3dd1cde5fdaa9
snek152 <kakanisnehil@gmail.com>
2022-05-30 08:35:17 -0700

    updating carousel images

--- lib/data/images.ts
+++ lib/data/images.ts
@@ -1,11 +1,6 @@
 export const homepageCarouselImages: string[] = [
   "/carousel/image1.jpeg",
-  "/carousel/image2.jpeg",
+  "/carousel/image2.png",
   "/carousel/image3.jpeg",
-];
-
-export const homepageImageCollage: string[] = [
-  "/collage/image1.jpeg",
-  "/collage/image2.jpeg",
-  "/collage/image3.jpeg",
+  "/carousel/image4.jpeg",
 ];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import About from "@components/pages/home/About";
 import Hero from "@components/pages/home/Hero";
-import Images from "@components/pages/home/Images";
 import Page from "@lib/types/Page";
 
 const Home: Page = () => {
@@ -8,7 +7,6 @@ const Home: Page = () => {
     <>
       <Hero />
       <About />
-      <Images />
     </>
   );
 };
