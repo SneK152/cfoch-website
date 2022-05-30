3badb4da959269c147e14c87348d9020ed289b40
snek152 <kakanisnehil@gmail.com>
2022-05-30 08:30:20 -0700

    adding a simple footer

--- components/layout/Container.tsx
+++ components/layout/Container.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@components/navbar/Navbar";
 import { ReactNode } from "react";
+import Footer from "./Footer";
 import Meta from "./Meta";
 
 interface ContainerProps {
@@ -14,6 +15,7 @@ export default function Container(props: ContainerProps) {
       <div className="bg-lightgreen h-16"></div>
       <Meta title={props.title} />
       <div className="bg-light">{props.children}</div>
+      <Footer />
     </>
   );
 }
