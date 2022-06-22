34649262f2adab61b8d8e04234f0985d56e9418e
snek152 <kakanisnehil@gmail.com>
2022-06-22 12:49:32 +0530

    adding necessary icons for seo

--- components/layout/Meta.tsx
+++ components/layout/Meta.tsx
@@ -11,12 +11,10 @@ export default function Meta(props: MetaProps) {
       <meta charSet="utf-8" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link rel="icon" href="/favicon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#000000" />
       <meta name="description" content={description} />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content="/logo.png" />
+      <meta property="og:image" content="/images/ms-icon-310x310.png" />
       <meta
         name="og:title"
         property="og:title"
@@ -41,8 +39,6 @@ export default function Meta(props: MetaProps) {
       />
       <meta name="format-detection" content="telephone=no" />
       <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="msapplication-TileColor" content="#2B5797" />
-      <meta name="msapplication-tap-highlight" content="no" />
       <meta name="theme-color" content="#000000" />
       <title>
         {title} | {props.title}
@@ -52,6 +48,82 @@ export default function Meta(props: MetaProps) {
       <meta name="robots" content="index, follow" />
       <meta name="language" content="English" />
       <meta name="revisit-after" content="14 days" />
+      <link
+        rel="apple-touch-icon"
+        sizes="57x57"
+        href="/images/apple-icon-57x57.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="60x60"
+        href="/images/apple-icon-60x60.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="72x72"
+        href="/images/apple-icon-72x72.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/images/apple-icon-76x76.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/images/apple-icon-114x114.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/images/apple-icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/images/apple-icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/images/apple-icon-152x152.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/images/android-icon-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/images/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/images/manifest.json" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta
+        name="msapplication-TileImage"
+        content="/images/ms-icon-144x144.png"
+      />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 }
