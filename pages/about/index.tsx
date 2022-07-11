ffe7e6fbd94424538de9717a0e7a9b83d85a9c04
snek152 <kakanisnehil@gmail.com>
2022-07-10 17:40:56 -0700

    fixing build error

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,8 +1,8 @@
 import Page from "@lib/types/Page";
-import LinkPage from "@components/layout/LinkPage";
+// import LinkPage from "@components/layout/LinkPage";
 
 const AboutPage: Page = () => {
-  return <LinkPage title="About" headingTitle="About Us" />;
+  return <></>;
 };
 
 AboutPage.title = "About";
